14b7907613b9ef373fdb8540c3d9bdc0e4ace933
gkrrywhtk2 <81000778+gkrrywhtk2@users.noreply.github.com>
2024-12-20 22:51:17 +0900

    <chore> 간단 체력바 수정
    <chore> 간단 체력바 수정

--- Assets/Scripts/Charaters/Player/Player_Status.cs
+++ Assets/Scripts/Charaters/Player/Player_Status.cs
@@ -4,6 +4,8 @@ public class Player_Status : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public bool isLive;
+    public float health;
+    public float maxHealth = 100;
     private void Awake() {
         isLive = true;
     }
