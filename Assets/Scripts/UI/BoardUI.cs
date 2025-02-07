56f03c784980cccd2f279bacd2dd6b4af5310580
gkrrywhtk2 <81000778+gkrrywhtk2@users.noreply.github.com>
2025-02-07 17:10:04 +0900

    <feat> UI 작업
    <feat> UI 작업

---
+++ Assets/Scripts/UI/BoardUI.cs
@@ -0,0 +1,28 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardUI : MonoBehaviour
+{
+   public GameObject[] boards;
+   //0 : upgrade, 1 : item ...
+   private void Awake() {
+     //Debug.Log(boards[0].GetComponent<RectTransform>().anchoredPosition);
+     HideAllBoards();
+     ShowBoard(2);//일단 카드 ui 호출
+   }
+
+   public void HideAllBoards(){
+    Vector3 hidePostion = new Vector3(-1500, 0, 0);
+    for(int i =0; i<boards.Length;i++){
+        boards[i].GetComponent<RectTransform>().anchoredPosition = hidePostion;
+    }
+   }
+
+   public void ShowBoard(int boardId){
+    Vector3 showPos = new Vector3(0,0,0);
+    HideAllBoards();
+    boards[boardId].GetComponent<RectTransform>().anchoredPosition = showPos;
+   }
+   }
+
